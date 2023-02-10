d1c076216b0ad4ad47cdd98a9cdf1a0263cc75d1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-02-10 21:13:09 +0600

    Add files via upload

---
+++ Number Theory/All Primes.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e8;
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<bool> sieve(N+1, true) ;
+    vector<int> primes;   //
+    sieve[0] = sieve[1] = false;
+
+    for(int i = 2; i*i <= N; i++){
+        if(sieve[i]){
+            primes.push_back(i);
+            for(int j = i*i; j <= N; j+=i){
+               sieve[j] = false;
+            }
+        }
+    }
+  }
+
+
+
+
+
+
+
+
+
