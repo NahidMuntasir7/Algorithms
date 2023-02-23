19e2fb3982fb46e7d8810767f6a3e083b46d2d71
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-02-23 20:45:30 +0600

    Update Optimized Sieve Spoj.cpp

--- Number Theory/Optimized Sieve Spoj.cpp
+++ Number Theory/Optimized Sieve Spoj.cpp
@@ -28,3 +28,41 @@ int main(){
       }
 
   }
+
+
+/* 
+ #include<bits/stdc++.h>
+ using namespace std;
+ 
+ #define ll long long
+ const int N = 1e6 + 7;
+
+vector<int> primes;
+
+void sievee(){
+    vector<bool> sieve(N+1, true) ;
+    sieve[0] = sieve[1] = false;
+
+    for(int i = 2; i*i <= N; i++){
+        if(sieve[i]){
+            for(int j = i*i; j <= N; j+=i){
+               sieve[j] = false;
+            }
+        }
+     }
+     for(int i = 2; i <= N; i++) {
+        if(sieve[i]) primes.push_back(i);
+    }
+}
+ 
+ int main(){
+
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     sievee();
+    
+     for(int i = 0; i < 10 ; i++){
+        cout << primes[i] << " ";
+     }
+ }
+*/
