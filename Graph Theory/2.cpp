68bbea78f8f573d32b81cd62420c17b0f130fa05
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-04 18:32:19 +0600

    Add files via upload

---
+++ Graph Theory/2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+#define ll long long
+#define endl '\n'
+const int inf = 1e7;
+    
+int main(){
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m; cin >> n >> m;
+
+    vector<int> dist(n + 1, inf);
+    vector<vector<pair<int, int>>> graph(n + 1);
+
+    for(int i = 0; i < m; i++){
+        int u, v, w; 
+        cin >> u >> v >> w;
+
+        graph[u].push_back({v, w});
+        graph[v].push_back({u, w}); //undirected
+    }
+
+    int source; cin >> source;
+
+    dist[source] = 0;
+
+    set<pair<int, int>> s;
+    s.insert({0, source});   // dis, source
+
+    while(!s.empty()){
+        auto x = *(s.begin());  
+        s.erase(x);
+
+        for(auto it : graph[x.second]){
+            if(dist[x.second] + it.second < dist[it.first]){ //amar distance + neigh. edge er weight < neigh. er distanse
+                s.erase({dist[it.first], it.first});         //done
+                dist[it.first] = dist[x.second] + it.second; // neigh. update
+                s.insert({dist[it.first], it.first});        // neigh. insert
+            }
+        } 
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(dist[i] < inf) cout << dist[i] << " ";
+        else cout << -1 << " "; 
+    }
+}
