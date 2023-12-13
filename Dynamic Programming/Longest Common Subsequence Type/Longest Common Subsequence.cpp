01c4ba0de3c804854b4fab42e5f2a6849e56e6a3
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-13 17:07:45 +0600

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence Type/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence Type/Longest Common Subsequence.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int LCSLength(string X, string Y, int m, int n){
+    if (m == 0 || n == 0) {
+        return 0;
+    }
+    if (X[m - 1] == Y[n - 1]) {
+        return LCSLength(X, Y, m - 1, n - 1) + 1;
+    }
+    return max(LCSLength(X, Y, m, n - 1), LCSLength(X, Y, m - 1, n));
+}
+ 
+int main(){
+    string X = "ABCBDAB", Y = "BDCABA";
+    cout << "The length of the LCS is " << LCSLength(X, Y, X.length(), Y.length());
+}
