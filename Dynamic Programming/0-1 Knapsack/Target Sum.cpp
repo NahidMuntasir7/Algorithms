f199b628758f9182b9a753de0caeb0500e953fc2
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-13 16:41:11 +0600

    Update Target Sum.cpp

--- Dynamic Programming/0-1 Knapsack/Target Sum.cpp
+++ Dynamic Programming/0-1 Knapsack/Target Sum.cpp
@@ -1,1 +1,15 @@
 // https://leetcode.com/problems/target-sum/solutions/3218461/most-easiest-way-c/
+
+class Solution {
+public:
+    int solve(vector<int> &nums, int target, int ans, int index){
+        if(index == nums.size() && ans == target) return 1;
+        if(nums.size() <= index) return 0;  // index == nums.size() is also ok
+
+        return solve(nums, target, ans + nums[index] , index + 1) + solve(nums, target, ans - nums[index], index + 1); //include and exclude
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int ans = 0, i = 0;
+        return solve(nums, target, ans, i);
+    }
+};
