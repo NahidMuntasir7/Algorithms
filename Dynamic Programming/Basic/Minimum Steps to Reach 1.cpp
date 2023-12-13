3a27ee36f261843c486a61c6e65a0def0e3c057a
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-13 17:43:38 +0600

    Update Minimum Steps to Reach 1.cpp

--- Dynamic Programming/Basic/Minimum Steps to Reach 1.cpp
+++ Dynamic Programming/Basic/Minimum Steps to Reach 1.cpp
@@ -26,3 +26,30 @@ int32_t main() {
   cout << min_steps(n) << '\n';
   return 0;
 }
+
+// 2
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 9;
+int step[N];
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n = 100000;
+  step[1] = 0;
+  for (int i = 2; i <= n; i++) {
+    int ans = step[i - 1] + 1;
+    if (i % 2 == 0) {
+      ans = min(ans, step[i / 2] + 1);
+    }
+    if (i % 3 == 0) {
+      ans = min(ans, step[i / 3] + 1);
+    }
+    step[i] = ans;
+  }
+  
+  cout << step[n] << '\n';
+  return 0;
+}
