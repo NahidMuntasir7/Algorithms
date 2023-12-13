2577674ad5ddab4922a964238bd679cdf93c60a8
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-13 17:30:05 +0600

    Update Longest Increasing Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence Type/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence Type/Longest Increasing Subsequence.cpp
@@ -1,3 +1,4 @@
+// 1
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -17,4 +18,31 @@ int main(){
     int n = sizeof(arr) / sizeof(arr[0]);
  
     cout << "The length of the LIS is " << LIS(arr, 0, n, INT_MIN); // LDS INT_MAX
+}
+
+// 2
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 10010;
+int a[N], dp[N];
+
+int32_t main() {
+  int n; cin >> n;
+  for (int i = 1; i <= n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 1; i <= n; i++) {
+    dp[i] = 1;
+    for (int j = 1; j < i; j++) {
+      if (a[j] < a[i]) {
+        dp[i] = max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ans = max(ans, dp[i]);
+  }
+  cout << ans << '\n';
 }
