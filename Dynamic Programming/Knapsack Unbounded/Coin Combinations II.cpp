351012ce321ed1c350ca72c1bf69958cd7a84dce
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-15 21:55:07 +0600

    Update Coin Combinations II.cpp

--- Dynamic Programming/Knapsack Unbounded/Coin Combinations II.cpp
+++ Dynamic Programming/Knapsack Unbounded/Coin Combinations II.cpp
@@ -33,3 +33,28 @@ int main(){
     }
     cout << func(1, x) << endl;
 }
+
+// **Accepted Version**
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll dp[1000001];
+const int MOD = (int)1e9 + 7;
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) { 
+        cin >> coins[i]; 
+    }
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int weight = 0; weight <= x; weight++) {
+            if (weight - coins[i - 1] >= 0) {  // prevent out of bounds cases
+                dp[weight] += dp[weight - coins[i - 1]];
+                dp[weight] %= MOD;
+            }
+        }
+    }
+    cout << dp[x] << '\n';
+}
