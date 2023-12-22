bc25b9a0b036f38109de7cb16f1786b8645dc889
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-22 15:42:46 +0600

    Update CSES-Minimizing Coins.cpp

--- Dynamic Programming/Knapsack Unbounded/CSES-Minimizing Coins.cpp
+++ Dynamic Programming/Knapsack Unbounded/CSES-Minimizing Coins.cpp
@@ -33,3 +33,36 @@ int main() {
     if(mini == INT_MAX) cout << -1 << endl;
     else cout << mini << endl;
 }
+
+// iterative
+#include<bits/stdc++.h>
+using namespace std; 
+ 
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    
+    vector<int> dp(x + 1, 1e9); // there maybe no ans
+    // initializing the dp values with a very high value initially
+    // dp[i] = min coins to generate a sum of i
+    
+    // base case
+    dp[0] = 0;
+    
+    for(int i = 1; i <= x; i++){
+        for(int j = 0; j < n; j++){
+            if(a[j] <= i){
+                // transition
+                dp[i] = min(dp[i], dp[i - a[j]] + 1);
+            }
+        }
+    }
+    
+    //final subproblem
+    cout << (dp[x] < 1e9 ? dp[x] : -1) << endl; // no ans check
+    return 0;
+
