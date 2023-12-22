b75ed2fb2ea0792a12a3b29a2613c36f86526959
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-22 16:32:04 +0600

    Update CSES-Dice Combinations.cpp

--- Dynamic Programming/Knapsack Unbounded/CSES-Dice Combinations.cpp
+++ Dynamic Programming/Knapsack Unbounded/CSES-Dice Combinations.cpp
@@ -63,3 +63,32 @@ int main(){
     //final subproblem
     cout << dp[n] << endl;
 }
+
+//iterative space-optimized
+#include<bits/stdc++.h>
+#define MOD 1000000007
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+ 
+    vector<int> prev = {1}; // only contains dp[0]
+    
+    for(int i = 1; i <= n; i++){
+        // finding dp[i] or number of ways to make a sum of i
+        // dp[i] = summation of all the previous states
+        int current = 0;
+        for(int j : prev){
+            current = (current + j) % MOD;
+        }
+        prev.push_back(current);
+        // if previous contains more than 6 elements
+        // discard the first element
+        if(prev.size() > 6){
+            prev.erase(prev.begin());
+        } 
+    }
+    //finally the last element of prev would be dp[n]
+    cout << prev.back() << endl;
+    return 0;
+}
