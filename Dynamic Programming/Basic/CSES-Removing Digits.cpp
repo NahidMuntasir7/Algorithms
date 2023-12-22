a426b4ce400ac57f3575a58fbce95f771a47bda1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-22 17:10:58 +0600

    Update CSES-Removing Digits.cpp

--- Dynamic Programming/Basic/CSES-Removing Digits.cpp
+++ Dynamic Programming/Basic/CSES-Removing Digits.cpp
@@ -26,3 +26,24 @@ int32_t main() {
     memset(dp, -1, sizeof dp);
     cout << min_steps(n) << '\n';
 }
+
+// iterative
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, 1e9);
+    dp[0] = 0;
+    for(int i = 1; i <= n; i++){
+        string a = to_string(i);
+        for(char c : a){
+            int digit = c - '0';
+            if(digit != 0){
+                dp[i] = min(dp[i], dp[i - digit] + 1);
+            }
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
