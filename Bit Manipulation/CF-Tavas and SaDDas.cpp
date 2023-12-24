125b42ec457201605960d2a720e0f847d707498e
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-24 11:27:02 +0600

    Create CF-Tavas and SaDDas.cpp

---
+++ Bit Manipulation/CF-Tavas and SaDDas.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+const int N = 1e6 + 7;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    vector<ll> anss;
+    for(int k = 1; k <= 10; k++){
+        for(int mask = 0; mask < (1 << k); mask++){
+            ll ans = 0;
+            for(int i = 0; i < k; i++){
+                if(mask & (1 << i)){
+                    ans *= 10;
+                    ans += 7;
+                }
+                else{
+                    ans *= 10;
+                    ans += 4;
+                }
+            }
+            anss.push_back(ans);
+        }
+    }
+    sort(anss.begin(), anss.end());
+
+    cout << lower_bound(anss.begin(), anss.end(), n) - anss.begin() + 1 << endl; // finding x in log(n) tc (1 based indexing)
+}
