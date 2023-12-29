c616bb494db4a993b7f9d0846af6713fa32e1565
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-29 13:52:56 +0600

    Create CSES - Subarray Sums II.cpp

---
+++ Others/Subarray/CSES - Subarray Sums II.cpp
@@ -0,0 +1,21 @@
+//Given an array of n integers, your task is to count the number of subarrays having sum x.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int n,x;
+    cin >> n >> x;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    long long sum = 0, cnt = 0;
+ 
+    map<long long, int> seen;
+    seen[0]++;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+        cnt += seen[sum - x];  // l <- x -> r so, r - l = x or, l = r - x.  current r er jonno suitable l ache kina check..
+        seen[sum]++;
+    }
+    cout << cnt << endl;
+}
