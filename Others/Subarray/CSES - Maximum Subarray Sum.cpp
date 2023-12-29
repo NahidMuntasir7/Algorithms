b57635bfcf20a9d8bc85975b3554ec957580d25f
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2023-12-29 14:00:17 +0600

    Create CSES - Maximum Subarray Sum.cpp

---
+++ Others/Subarray/CSES - Maximum Subarray Sum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+#define ll long long
+#define endl '\n'
+const int N = 1e6 + 7;
+    
+int main(){
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll n;
+    cin >> n;
+    ll arr[n];
+ 
+    for(ll i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    ll best = INT_MIN, sum = 0;
+ 
+    for(ll i = 0; i < n; i++){
+        sum = max(sum + arr[i], arr[i]);   //... -1 -5 -2 ... in this case we will take -2 ...
+        best = max(best, sum);  // taking max of all changing sum ...
+    }
+    cout << best << endl;
+}
