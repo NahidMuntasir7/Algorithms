ee0febafcf3016d6fd2cfa73367c9b67d0641c92
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-13 22:21:13 +0600

    Update Re 8 - Combination Sum.cpp

--- Recursion/Re 8 - Combination Sum.cpp
+++ Recursion/Re 8 - Combination Sum.cpp
@@ -1,1 +1,26 @@
+// leetcode - Combination sum
 
+class Solution{
+  public:
+    void findCombination(int i, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
+      if(i == arr.size()){
+        if(target == 0){
+          ans.push_back(ds);
+        }
+      }
+      if(arr[i] <= target){ // to prevent negative value
+        ds.push_back(arr[i]); // 1
+        findCombination(i, target - arr[i], arr, ans, ds);
+        ds.pop_back(); // 2
+      }
+      findCombination(i + 1, target, arr, ans, ds);
+    }
+
+  public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target){
+      vector<vector<int>> ans;
+      vector<int> ds;
+      findCombination(0, target, candidates, ans, ds);
+      return ans;
+    }
+};
