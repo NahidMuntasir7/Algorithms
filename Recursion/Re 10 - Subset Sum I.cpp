9d3d5c99e111993ea178c816bef656ea7547067b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-02-14 21:17:34 +0600

    Update Re 10 - Subset Sum I.cpp

--- Recursion/Re 10 - Subset Sum I.cpp
+++ Recursion/Re 10 - Subset Sum I.cpp
@@ -25,7 +25,7 @@ class Solution {
 
 
 int main() {
-  vector < int > arr{3,1,2};
+  vector < int > arr{3, 1, 2};
   Solution ob;
   vector < int > ans = ob.subsetSums(arr, arr.size());
   sort(ans.begin(), ans.end());
@@ -34,6 +34,5 @@ int main() {
     cout << sum << " ";
   }
   cout << endl;
-
   return 0;
 }
