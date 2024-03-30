b5fbbf134e8c62d8f166a9a8f3ea40d050ae2d5d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-30 17:03:22 +0600

    Update L2. Maximum Points You Can Obtain from Cards.cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L2. Maximum Points You Can Obtain from Cards.cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L2. Maximum Points You Can Obtain from Cards.cpp
@@ -1,1 +1,38 @@
-// 
+// given a array of cards.. you can pick k cards .. serially from leftmost to right and / or rightmost to left
+// let k == 4
+// [(4, 3), 6, 4, 7, 2, (1, 7)] or [(4, 3, 6, 4), 7, 2, 1, 7] or [4, 3, 6, 4, (7, 2, 1, 7)] or [(4, 3, 6), 4, 7, 2, 1, (7)] .... like this
+// what is the maxx sum?
+
+// make a left sum of first k elements and make a right sum of 0.. then sub from left and add to right
+// 7 3
+// 7 2 3 1 1 4 5
+// ans = 16; (7) 2 3 1 1 (4 5)
+  
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n, k; cin >> n; cin >> k;
+  int arr[n];
+
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  
+  int lsum = 0;
+  for(int i = 0; i < k; i++){
+      lsum += arr[i];
+  }
+
+  int mx = lsum, right = n - 1, rsum = 0;
+
+  for(int i = k - 1; i >= 0; i--){  
+      lsum -= arr[i];
+      rsum += arr[right--];
+      mx = max(mx, lsum + rsum);  // substracting from lsum (right to left) and adding to rsum (right to left)
+  }
+
+  cout << mx;
+
+}
