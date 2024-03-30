7278938bfcab6b8f5d7d056a7825005634b3ee21
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-30 21:37:42 +0600

    Update L5. Fruit Into Baskets.cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L5. Fruit Into Baskets.cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L5. Fruit Into Baskets.cpp
@@ -1,1 +1,20 @@
+// LeetCode - Fruit Into Baskets
+// maximum length of subarray with at most 2 types of numbers
 
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int ans = 0;
+        unordered_map<int,int> mp;
+        for(int left = 0, right = 0; right < fruits.size(); right++){
+            mp[fruits[right]]++;
+            while(mp.size() > 2){
+                mp[fruits[left]]--;
+                if(mp[fruits[left]] == 0) mp.erase(fruits[left]); //
+                left++;
+            }
+            ans = max(ans,right - left + 1);
+        }
+        return ans;
+    }
+};
