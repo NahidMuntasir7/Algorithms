9bc40d96537699d768e7d32dd5cc4003f72e570a
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-31 19:46:08 +0600

    Update L10. Count number of Nice subarrays.cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L10. Count number of Nice subarrays.cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L10. Count number of Nice subarrays.cpp
@@ -1,1 +1,28 @@
+// Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+// Return the number of nice sub-arrays.
 
+// 
+class Solution {
+public:
+    int subArray(vector<int>& nums, int k) {
+        int count = 0, ans = 0, start = 0, end = 0;
+        int n = nums.size();
+        while(end < n){
+            if(nums[end] % 2 == 1){
+                count++;
+            }
+            while(count > k){
+                if(nums[start] % 2 == 1){
+                    count--;
+                }
+                start++;
+            }
+            ans += end - start + 1;
+            end++;
+        }
+        return ans;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+      return subArray(nums, k) - subArray(nums, k - 1);
+    }
+};
