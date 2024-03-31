b06ab81da45f09fd25042c891d800790f97e1289
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-31 14:02:06 +0600

    Update L8. Longest Repeating Character Replacement*.cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L8. Longest Repeating Character Replacement*.cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L8. Longest Repeating Character Replacement*.cpp
@@ -2,7 +2,8 @@
 // You can perform this operation at most k times.
 // Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
-// ** can be written as - longest subarray where the number of different character is <= k **
+// ** can be written as - longest subarray where the number of different character without the max occured character is <= k **
+// i can use if instead of second while loop
 
 
 class Solution {
@@ -14,8 +15,10 @@ public:
         while (i < s.length()) {
             count[s[i] - 'A']++;
             maxCount = max(maxCount, count[s[i] - 'A']);
-            while(i - j + 1 - maxCount > k) { // number of different char = length - max frequency
-                count[s[j] - 'A']--;        // condition (diff <= k)
+            while(i - j + 1 - maxCount > k) { // number of different char without max freq = length - max frequency
+                count[s[j] - 'A']--;          // condition (diff char <= k)
+                maxCount = 0;
+                for(int i = 0; i < 26; i++) maxCount = max(maxCount, count[i]); // updating max count      
                 j++;
             }
             result = max(result, i - j + 1);
