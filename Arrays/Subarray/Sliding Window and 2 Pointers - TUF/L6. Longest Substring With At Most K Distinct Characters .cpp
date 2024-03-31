3425beee2bc16bb976be9c054f19804477ef0c1d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-31 10:40:40 +0600

    Update L6. Longest Substring With At Most K Distinct Characters .cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L6. Longest Substring With At Most K Distinct Characters .cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L6. Longest Substring With At Most K Distinct Characters .cpp
@@ -1,1 +1,29 @@
+// Find the length of the longest substring with at most k distinct characters (cnt <= k)
+// like the previous one
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        unordered_map<char, int> mp; 
+      
+        int mx = 0; 
+        int left = 0; 
+        int n = s.size(); 
+      
+        for (int right = 0; right < n; right++) {
+            mp[s[right]]++;
+
+            while (mp.size() > k) {
+                mp[s[left]]--;
+                if (mp[s[left]] == 0) {
+                    mp.erase(s[left]);
+                }
+                left++;
+            }
+            mx = max(mx, right - left + 1);
+        }
+        return mx;
+    }
+};
+ 
+
 
