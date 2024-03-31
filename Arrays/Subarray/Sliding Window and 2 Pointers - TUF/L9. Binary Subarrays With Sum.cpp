6b61ca1331326979cf143d86c8d3bf0ca1b75c62
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-03-31 19:05:05 +0600

    Update L9. Binary Subarrays With Sum.cpp

--- Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L9. Binary Subarrays With Sum.cpp
+++ Arrays/Subarray/Sliding Window and 2 Pointers - TUF/L9. Binary Subarrays With Sum.cpp
@@ -20,9 +20,29 @@ public:
 };
 
 
-// Approach 2
+// *** Approach 2 ***
 
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return SUMARRAY(nums, goal) - SUMARRAY(nums, goal - 1);
+    }
+    
+    int SUMARRAY(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
 
-
+        int count = 0, i = 0, j = 0, sum = 0;
+        while (i < nums.size()) {
+            sum += nums[i]; 
+            while (sum > goal) {
+                sum -= nums[j];
+                j++;
+            }
+            count += i - j + 1;
+            i++;
+        }
+        return count;
+    }
+};
 
 
