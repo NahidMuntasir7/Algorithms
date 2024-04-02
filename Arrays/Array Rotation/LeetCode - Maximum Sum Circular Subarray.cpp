ebac134cb11e1aff042875b716cd262ea19ab23b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-02 11:31:08 +0600

    Update LeetCode - Maximum Sum Circular Subarray.cpp

--- Arrays/Array Rotation/LeetCode - Maximum Sum Circular Subarray.cpp
+++ Arrays/Array Rotation/LeetCode - Maximum Sum Circular Subarray.cpp
@@ -7,7 +7,7 @@ public:
             totalsum += nums[i];
 
             sum1 = sum1 + nums[i];   
-            maxi1 = max(maxi1, sum1);  // maximum
+            maxi1 = max(maxi1, sum1);  // maximum kadane
             if(sum1 < 0) sum1 = 0;
         }  
 
@@ -15,10 +15,10 @@ public:
  
          for(int i = 0; i < nums.size(); i++){
             sum2 = sum2 + nums[i] * (-1);   
-            maxofmini = max(maxofmini, sum2);  // minimum
+            maxofmini = max(maxofmini, sum2);  // minimum kadane
             if(sum2 < 0) sum2 = 0;
         } 
-        int maxi2 = totalsum - maxofmini * (-1); // total - minimum => circular
+        int maxi2 = totalsum - maxofmini * (-1); // total - maxofmini => circular maxi
         
         if(maxi2 == 0) return maxi1; // empty subarray... when all negative (totalsum - maxofmini == 0)
         return max(maxi1, maxi2);                                        // -3 -5 -1 here, total sum = -9, max of mini = -9 so, ans = 0; no subarray will be taken  
