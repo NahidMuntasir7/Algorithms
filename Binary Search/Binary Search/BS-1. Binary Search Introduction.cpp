a277cdd8ad52000de2d1156689030c20184547cb
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-03 21:41:43 +0600

    Update BS-1. Binary Search Introduction.cpp

--- Binary Search/Binary Search/BS-1. Binary Search Introduction.cpp
+++ Binary Search/Binary Search/BS-1. Binary Search Introduction.cpp
@@ -1,7 +1,7 @@
-//  when elenent is not in the array ->
+//  *** when elenent is not in the array ***
 //  1 2 3 (4) (6) 7
 //  0 1 2  3   4  5
-//  for element 5: index of -> low = 4 and high = 3
+//  for element 5: index of -> low = 4 and high = 3 | ** then the loop breaks **
 
 
 //  overflow case: mid = lo + (hi - lo) / 2
