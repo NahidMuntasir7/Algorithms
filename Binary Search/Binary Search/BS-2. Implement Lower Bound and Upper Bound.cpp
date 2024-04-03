2853bd9b7e69b335b9bc8075584739173bc2b38c
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-03 21:26:27 +0600

    Update BS-2. Implement Lower Bound and Upper Bound.cpp

--- Binary Search/Binary Search/BS-2. Implement Lower Bound and Upper Bound.cpp
+++ Binary Search/Binary Search/BS-2. Implement Lower Bound and Upper Bound.cpp
@@ -1,1 +1,8 @@
+// lower bound  ->  arr[i] >= n | arr[i] is the smallest indexed one
+// arr[] = 3, 5, 8, 15, 19, 19, 19
+// let n = 8  so, lower bound = 8 
+// let n = 9  so, lower bound = 15 
+// let n = 19 so, lower bound = 19(1st occurence)
+// let n = 20..  lower bound = last index + 1 / size of the array
 
+// 
