86395851c22eab185f901eb82a4f7ac2612f160e
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-04 13:11:53 +0600

    Update BS-3. First and Last Occurrences in Array.cpp

--- Binary Search/Binary Search/BS-3. First and Last Occurrences in Array.cpp
+++ Binary Search/Binary Search/BS-3. First and Last Occurrences in Array.cpp
@@ -1,1 +1,16 @@
+// first and last occurance of a given number
+
+// first occurence = lower bound
+// last  occurence = upper bound - 1
+
+// edge cases
+
+// first occurence and last occurence
+
+  if(lb == n or arr[lb] != x) lb and ub not present
+  else{
+        first = lb;
+        last = ub - 1
+   }
+
 
