00ab41c58bf207c83ff2d4d253c70470796376fa
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-04 15:36:18 +0600

    Update BS-3. First and Last Occurrences in Array.cpp

--- Binary Search/Binary Search/BS-3. First and Last Occurrences in Array.cpp
+++ Binary Search/Binary Search/BS-3. First and Last Occurrences in Array.cpp
@@ -7,6 +7,11 @@
 
 // first occurence and last occurence
 
+// logic: if lb == n: the element is not present so, if not present then no first last 
+// if lb present may not be equal to x
+
+// if lb is present and equal to x.. then always, last occ = ub - 1 think..
+
   if(lb == n or arr[lb] != x) lb and ub not present
   else{
         first = lb;
