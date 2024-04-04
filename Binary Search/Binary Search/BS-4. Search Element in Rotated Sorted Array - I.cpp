37e5dd2428b424205566969ed9ed94d75a6d12a3
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-04 21:45:44 +0600

    Update BS-4. Search Element in Rotated Sorted Array - I.cpp

--- Binary Search/Binary Search/BS-4. Search Element in Rotated Sorted Array - I.cpp
+++ Binary Search/Binary Search/BS-4. Search Element in Rotated Sorted Array - I.cpp
@@ -3,19 +3,24 @@
 
 // the array will be having ** unique eliments **
 
-int low = 0, high = n - 1;
 
-while(low <= high){
-    mid = (low + high) / 2;
-    if(arr[mid] == target) return mid;
-    // left sorted
-    if(arr[low] <= arr[mid]){
-        if(arr[low] <= target and target <= arr[mid]) high = mid - 1;
-        else low = mid + 1;
+int search(vector<int>& arr, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        
+        if (arr[mid] == k) return mid; //if mid points the target
+
+        //if left part is sorted
+        if (arr[low] <= arr[mid]) {
+            if(arr[low] <= k and k <= arr[mid]) high = mid - 1; //element exists
+            else low = mid + 1; //element does not exist
+        }
+        //if right part is sorted
+        else{ 
+            if(arr[mid] <= k and k <= arr[high]) low = mid + 1;
+            else  high = mid - 1;
+        }
     }
-    //right sorted
-    else{
-        if(arr[mid] <= target and target <= arr[high]) low = mid + 1;
-        else high = mid - 1;
-    }
+    return -1;
 }
