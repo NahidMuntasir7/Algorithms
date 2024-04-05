199d65562fbc82dee5fd134b723a1af7f127bec7
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-06 00:11:59 +0600

    Update BS-8. Single Element in Sorted Array.cpp

--- Binary Search/Binary Search/BS-8. Single Element in Sorted Array.cpp
+++ Binary Search/Binary Search/BS-8. Single Element in Sorted Array.cpp
@@ -4,7 +4,7 @@
 // findout that number
 
 // [1, 1, 2, 2, 3, 3, (4), 5, 5, 6, 6]
-//  0  1  2  3  4  5   6   7  8  9  10
+//  0  1  2  3  4  5   6   7  8  9  10 (index)
 int singleNonDuplicate(vector<int>& arr) {
     int n = arr.size(); 
 
