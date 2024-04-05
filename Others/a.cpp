0ffa97746913802a116a8940166ae275a50d40aa
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-05 20:52:19 +0600

    Update a.cpp

--- Others/a.cpp
+++ Others/a.cpp
@@ -1,1 +1,21 @@
+int main() {
 
+    int n,ans;
+    cin >> n;
+ 
+ 
+    if(n % 2 == 0 && (n / 2) % 2 == 0){
+        cout << n / 2 << " " << n / 2 << endl;
+    }
+    else if(n % 2 == 0 && (n/2) % 2 != 0){
+        cout << n / 2 - 1 << " " << n / 2 + 1 << endl;
+    }
+ 
+    else if(n % 2 != 0){
+ 
+       ans = 3 * (3 + 2 * (n / 10 - 1));
+ 
+       cout << n - ans << " " << ans << endl;
+    }
+ 
+ }
