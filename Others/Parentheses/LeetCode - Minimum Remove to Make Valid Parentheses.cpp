0ab1f6b21f3b06360ce9127caabcef548cacf2c5
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-06 16:52:41 +0600

    Update LeetCode - Minimum Remove to Make Valid Parentheses.cpp

--- Others/Parentheses/LeetCode - Minimum Remove to Make Valid Parentheses.cpp
+++ Others/Parentheses/LeetCode - Minimum Remove to Make Valid Parentheses.cpp
@@ -10,7 +10,7 @@ public:
         for(int i = 0; i < s.size(); i++){
             if(s[i] == ')' and st.empty()) s[i] = '@';   // beats )( and extra closing ')'
             else if(s[i] == '(') st.push(i);  // beats extra opening '(' by storing its position
-            else if(s[i] == ')' and !st.empty()) st.pop(); // valid
+            else if(s[i] == ')' and !st.empty()) st.pop(); // valid (pop condition)
         }   
 
         while(!st.empty()){ // if extra ((( remains eliminate them
