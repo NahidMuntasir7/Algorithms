98b6e52234f9a3ce6d94d4dfaf1a1df39f107489
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-06 16:44:38 +0600

    Update Minimum Remove to Make Valid Parentheses.cpp

--- Others/Parentheses/Minimum Remove to Make Valid Parentheses.cpp
+++ Others/Parentheses/Minimum Remove to Make Valid Parentheses.cpp
@@ -1,1 +1,27 @@
+// three condition for a invalid Parentheses:
+// 1. )(
+// 2. extra closing
+// 3. extra opening
 
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ')' and st.empty()) s[i] = '@';   // beats )( and extra closing ')'
+            else if(s[i] == '(') st.push(i);  // beats extra opening '(' by storing its position
+            else if(s[i] == ')' and !st.empty()) st.pop(); // valid
+        }   
+
+        while(!st.empty()){ // if extra ((( remains eliminate them
+            s[st.top()] = '@';
+            st.pop();
+        }
+        string ss = "";
+        for(auto it: s){
+            if(it == '@') continue;
+            ss += it;
+        }
+        return ss;
+    }
+};
