66a9e5d0262c8e2ef30e2ae7772017f5594ddcf1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-06 19:40:21 +0600

    Update BS-10. Finding Sqrt of a number using Binary Search.cpp

--- Binary Search/Binary Search/BS-10. Finding Sqrt of a number using Binary Search.cpp
+++ Binary Search/Binary Search/BS-10. Finding Sqrt of a number using Binary Search.cpp
@@ -1,1 +1,20 @@
+// alternative: in-built sqrt() function: int ans = sqrt(n);
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int n) {
+    int low = 1, high = n;     // binary search on the answers
+
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            low = mid + 1;  // eliminate the left half
+        }
+        else {
+            high = mid - 1; // eliminate the right half
+        }
+    }
+    return high;
+}
