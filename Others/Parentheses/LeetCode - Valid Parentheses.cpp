e3e4f194dc4bf8adaf0999ce8e8c616410a0c892
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-06 16:51:35 +0600

    Update LeetCode - Valid Parentheses.cpp

--- Others/Parentheses/LeetCode - Valid Parentheses.cpp
+++ Others/Parentheses/LeetCode - Valid Parentheses.cpp
@@ -1,10 +1,15 @@
+// three condition for a invalid Parentheses:
+// 1. )(
+// 2. extra closing
+// 3. extra opening
+
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
         int flag = 0;
         for(int i = 0; i < s.size(); i++){
-            if(st.empty()) st.push(s[i]);  // beats "()]"
+            if(st.empty()) st.push(s[i]);  // beats "()]" 
             else if((s[i] == ')' and st.top() == '(') or (s[i] == ']' and st.top() == '[') or (s[i] == '}' and st.top() == '{')){
                 if(!st.empty()) st.pop(); //pop condition
             }
