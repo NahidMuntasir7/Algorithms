8b27c34cf8320c35f481e4eec9a2edf7af488852
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-07 12:11:30 +0600

    Update BS-12. Koko Eating Bananas.cpp

--- Binary Search/Binary Search/BS-12. Koko Eating Bananas.cpp
+++ Binary Search/Binary Search/BS-12. Koko Eating Bananas.cpp
@@ -1,1 +1,13 @@
-//
+// Problem Statement: A monkey is given ‘n’ piles of bananas, whereas the 'ith' pile has ‘a[i]’ bananas. An integer ‘h’ is also given, 
+// which denotes the time (in hours) for all the bananas to be eaten.
+// Each hour, the monkey chooses a non-empty pile of bananas and eats ‘k’ bananas. If the pile contains less than ‘k’ bananas, then 
+// the monkey consumes all the bananas and won’t eat any more bananas in that hour. (eats ------> this way)
+
+// Find the minimum number of bananas ‘k’ to eat per hour so that the monkey can eat all the bananas within ‘h’ hours.
+
+// Example 1:
+// Input Format:
+//  N = 4, a[] = {7, 15, 6, 3}, h = 8
+// Result:
+// 5
+
