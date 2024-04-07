6ca32ac994fb0eb2c699dfb4fb8899e61279f6fb
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-07 12:39:43 +0600

    Update BS-13. Minimum days to make M bouquets.cpp

--- Binary Search/Binary Search/BS-13. Minimum days to make M bouquets.cpp
+++ Binary Search/Binary Search/BS-13. Minimum days to make M bouquets.cpp
@@ -1,1 +1,7 @@
+// You are given 'n' roses and you are also given an array 'arr' where 'arr[i]' denotes that the 'ith' rose will bloom on the 'arr[i]th' day.
+// You can only pick already bloomed roses that are adjacent to make a bouquet. You are also told that you require exactly 'k' adjacent bloomed roses to make a single bouquet.
+// Find the minimum number of days required to make at least 'm' bouquets each containing 'k' roses. Return -1 if it is not possible.
 
+// Example :
+// Input: n = 9,  arr = [ 1, 2, 1, 2, 7, 2, 2, 3, 1 ], k = 3, m = 2
+// Output: 3
