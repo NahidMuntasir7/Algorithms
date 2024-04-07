1c1e5e40c0ad1b3d586f2684645ed1aed7d99631
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-07 20:45:04 +0600

    Update BS-14. Find the Smallest Divisor Given a Threshold.cpp

--- Binary Search/Binary Search/BS-14. Find the Smallest Divisor Given a Threshold.cpp
+++ Binary Search/Binary Search/BS-14. Find the Smallest Divisor Given a Threshold.cpp
@@ -1,1 +1,11 @@
+// Problem statement
+// You are given an array of integers 'arr' and an integer 'limit'.
+// Your task is to find the smallest positive integer divisor, such that upon dividing all the elements of the given array by it, 
+// the sum of the division's result is less than or equal to the given integer's limit.
 
+// Sample Input 1 :
+// 5
+// 1 2 3 4 5
+// 8  
+// Sample Output 1 :
+// 3
