fa15054473f9a31c49b06414f4a0a7cf20cfdcf1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-08 11:31:23 +0600

    Update BS-15. Capacity to Ship Packages within D Days.cpp

--- Binary Search/Binary Search/BS-15. Capacity to Ship Packages within D Days.cpp
+++ Binary Search/Binary Search/BS-15. Capacity to Ship Packages within D Days.cpp
@@ -1,1 +1,11 @@
+// Problem statement
+// You are the owner of a Shipment company. You use conveyor belts to ship packages from one port to another. The packages must be shipped within 'd' days.
+// The weights of the packages are given in an array 'weights'. The packages are loaded on the conveyor belts every day in the same order as they appear in the array. 
+// The loaded weights must not exceed the maximum weight capacity of the ship.
 
+// Find out the least-weight capacity so that you can ship all the packages within 'd' days.
+// Sample Input 1:
+// 8 5
+// 5 4 5 2 3 4 5 6
+// Sample Output 1:
+// 9
