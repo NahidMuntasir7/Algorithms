1076b0616bb3c0d0da55ea31012555d157c1a06d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-08 18:48:48 +0600

    Update BS-15. Capacity to Ship Packages within D Days.cpp

--- Binary Search/Binary Search/BS-15. Capacity to Ship Packages within D Days.cpp
+++ Binary Search/Binary Search/BS-15. Capacity to Ship Packages within D Days.cpp
@@ -9,3 +9,38 @@
 // 5 4 5 2 3 4 5 6
 // Sample Output 1:
 // 9
+
+
+class Solution {
+public:
+
+    int findDays(vector<int>& weights, int capacity){
+        int Day = 1, load = 0;
+        for(int i = 0; i < weights.size(); i++){
+            if(load + weights[i] > capacity){
+                Day += 1;
+                load = weights[i];
+            }
+            else{
+                load += weights[i];
+            }
+        }
+        return Day;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int noOfDays = findDays(weights,mid);
+            if(noOfDays <= days){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
