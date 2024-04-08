af9fb24de5d9a2367fea51269f1a5887ba5b70be
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-08 19:47:59 +0600

    Create BS-17. Aggressive Cows.cpp

---
+++ Binary Search/Binary Search/BS-17. Aggressive Cows.cpp
@@ -0,0 +1,55 @@
+// Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1 ... xN (0 <= xi <= 1,000,000,000).
+// His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, 
+// FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+
+// input: 
+// 1
+// 5 3
+// 1
+// 2
+// 8
+// 4
+// 9
+// output: 
+// 3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1, high = stalls[n - 1] - stalls[0];
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) {
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return high;
+}
+
+int main(){
+    vector<int> stalls = {0, 3, 4, 7, 10, 9};
+    int k = 4;
+    int ans = aggressiveCows(stalls, k);
+    cout << "The maximum possible minimum distance is: " << ans << "\n";
+    return 0;
+}
