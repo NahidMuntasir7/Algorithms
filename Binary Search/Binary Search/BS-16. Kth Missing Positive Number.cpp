1b3decdff2f0f78be7d02605db20cc6f9f09d1f3
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-08 19:07:05 +0600

    Create BS-16. Kth Missing Positive Number.cpp

---
+++ Binary Search/Binary Search/BS-16. Kth Missing Positive Number.cpp
@@ -0,0 +1,8 @@
+// Problem statement
+// You are given a strictly increasing array 'vec' and a positive integer 'k'.
+// Find the 'kth' positive integer missing from 'vec'.
+
+// Example :
+// Input: vec = [2,4,5,7] , k = 3
+// Output: 6
+
