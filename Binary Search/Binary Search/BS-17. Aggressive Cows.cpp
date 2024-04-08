d3b015332e92d945db857deccf6373d22242a7f9
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-08 19:54:00 +0600

    Update BS-17. Aggressive Cows.cpp

--- Binary Search/Binary Search/BS-17. Aggressive Cows.cpp
+++ Binary Search/Binary Search/BS-17. Aggressive Cows.cpp
@@ -2,6 +2,8 @@
 // His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, 
 // FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the ** largest minimum distance ** ?
 
+// minimum distance between cows is max
+
 // input: 
 // 1
 // 5 3
