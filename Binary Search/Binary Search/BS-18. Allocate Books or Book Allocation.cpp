f3c6134c937ca1173409252e97bd2440c35d2fb7
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-22 12:36:21 +0600

    Update BS-18. Allocate Books or Book Allocation.cpp

--- Binary Search/Binary Search/BS-18. Allocate Books or Book Allocation.cpp
+++ Binary Search/Binary Search/BS-18. Allocate Books or Book Allocation.cpp
@@ -1,1 +1,16 @@
-// 
+// Problem statement:
+// Given an array ‘arr’ of integer numbers, ‘arr[i]’ represents the number of pages in the ‘i-th’ book.
+// There are ‘m’ number of students, and the task is to allocate all the books to the students.
+
+// Allocate books in such a way that:
+// 1. Each student gets at least one book.
+// 2. Each book should be allocated to only one student.
+// 3. Book allocation should be in a contiguous manner.
+
+// You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum.
+// If the allocation of books is not possible, return -1.
+
+// Example:
+// Input: ‘n’ = 4 ‘m’ = 2 
+// ‘arr’ = [12, 34, 67, 90]
+// Output: 113
