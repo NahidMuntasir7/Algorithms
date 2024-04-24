b41d76044902dfef5d0f97927301c198d2d06ace
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-24 11:41:28 +0600

    Update BS 19. Painter's Partition and Split Array - Largest Sum.cpp

--- Binary Search/Binary Search/BS 19. Painter's Partition and Split Array - Largest Sum.cpp
+++ Binary Search/Binary Search/BS 19. Painter's Partition and Split Array - Largest Sum.cpp
@@ -8,3 +8,20 @@
 // Example :
 // Input: arr = [2, 1, 5, 6, 2, 3], k = 2
 // Output: 11
+
+
+// Largest Subarray Sum Minimized
+
+// Given an integer array ‘A’ of size ‘N’ and an integer ‘K'.
+// Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized.
+// Your task is to return the minimized largest sum of the split.
+// A subarray is a contiguous part of the array.
+
+// Example:
+// Input: ‘N’ = 5, ‘A’ = [1, 2, 3, 4, 5], ‘K’ = 3
+// Output: 6
+
+// Explanation: There are many ways to split the array ‘A’ into K consecutive subarrays. 
+// The best way to do this is to split the array ‘A’ into [1, 2, 3], [4], and [5], where the largest sum among the three subarrays is only 6.
+
+
