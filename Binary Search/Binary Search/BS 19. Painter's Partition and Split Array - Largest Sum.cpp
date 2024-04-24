112f7dd44c63abb80b54a370b66e3f870dfdfe7d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-24 11:38:42 +0600

    Update BS 19. Painter's Partition and Split Array - Largest Sum.cpp

--- Binary Search/Binary Search/BS 19. Painter's Partition and Split Array - Largest Sum.cpp
+++ Binary Search/Binary Search/BS 19. Painter's Partition and Split Array - Largest Sum.cpp
@@ -1,1 +1,10 @@
+// Painter's Partition Problem
 
+// Given an array/list of length ‘n’, where the array/list represents the boards and each element of the given array/list represents the length of each board. 
+// Some ‘k’ numbers of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint.
+// You are supposed to return the area of the minimum time to get this job done of painting all the ‘n’ boards under a constraint that 
+// any painter will only paint the continuous sections of boards.
+
+// Example :
+// Input: arr = [2, 1, 5, 6, 2, 3], k = 2
+// Output: 11
