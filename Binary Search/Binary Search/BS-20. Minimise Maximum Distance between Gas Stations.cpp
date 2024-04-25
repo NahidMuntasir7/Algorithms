85e3d41e74d5412d6cfadfd403efbbcffee34dc4
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-25 12:03:41 +0600

    Update BS-20. Minimise Maximum Distance between Gas Stations.cpp

--- Binary Search/Binary Search/BS-20. Minimise Maximum Distance between Gas Stations.cpp
+++ Binary Search/Binary Search/BS-20. Minimise Maximum Distance between Gas Stations.cpp
@@ -4,8 +4,8 @@
 // You are also given an integer ‘k’.
 // You have to place 'k' new gas stations on the X-axis.
 // You can place them anywhere on the non-negative side of the X-axis, even on non-integer positions.
-// Let 'dist' be the maximum value of the distance between adjacent gas stations after adding 'k' new gas stations.
 
+// Let 'dist' be the maximum value of the distance between adjacent gas stations after adding 'k' new gas stations.
 // Find the minimum value of dist.
 
 // Example:
