9dede51e680c4cec75cd67d8fdcf4f275316155e
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-27 18:44:41 +0600

    Create G-9. Flood Fill Algorithm.cpp

---
+++ Graph Theory/Graph Series/G-9. Flood Fill Algorithm.cpp
@@ -0,0 +1,6 @@
+// Problem Statement: An image is represented by a 2-D array of integers, each integer representing the pixel value of the image. Given a coordinate (sr, sc) 
+// representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+// To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same colour as the starting pixel, 
+// plus any pixels connected 4-directionally to those pixels (also with the same colour as the starting pixel), and so on. 
+// Replace the colour of all of the aforementioned pixels with the newColor.
