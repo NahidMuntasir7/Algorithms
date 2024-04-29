14c4fc520bac54c3c3241ebb1c98cbabf6d2c7a4
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-29 17:04:12 +0600

    Update G-9. Flood Fill Algorithm.cpp

--- Graph Theory/Graph Series/G-9. Flood Fill Algorithm.cpp
+++ Graph Theory/Graph Series/G-9. Flood Fill Algorithm.cpp
@@ -81,3 +81,37 @@ public:
         return image;
     }
 };
+
+
+// given data te no change korle bhalo... tai extra ans matrix use kora better.......
+// DFS e ans matrix added.. BFS same..
+class Solution {
+public:
+
+    void dfs(int row, int col, int color,int initColor, vector<vector<int>>& ans, vector<vector<int>>& image,int delRow[],int delCol[]){
+    
+        int n = image.size();
+        int m = image[0].size();
+
+        ans[row][col]= color;
+        for(int i = 0; i < 4; i++){
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol] == initColor && ans[nRow][nCol] != color){
+                dfs(nRow, nCol, color, initColor, ans, image,delRow, delCol);
+            }
+        }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>> & image, int sr, int sc, int color) {
+        
+        vector<vector<int>> ans = image;
+        int initColor = image[sr][sc];
+        int delRow[] = {-1, +1, 0, 0};
+        int delCol[] = {0, 0, 1, -1};
+        dfs(sr, sc, color, initColor, ans, image, delRow, delCol);
+        
+        return ans;
+
+    }
+};
