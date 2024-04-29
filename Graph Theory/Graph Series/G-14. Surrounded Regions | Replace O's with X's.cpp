0a444e537c142308a5e8933f9fd18108285b18fd
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-29 21:04:42 +0600

    Update G-14. Surrounded Regions | Replace O's with X's.cpp

--- Graph Theory/Graph Series/G-14. Surrounded Regions | Replace O's with X's.cpp
+++ Graph Theory/Graph Series/G-14. Surrounded Regions | Replace O's with X's.cpp
@@ -15,8 +15,7 @@ private:
             int nrow = row + delrow[i];
             int ncol = col + delcol[i]; 
             // check for valid coordinates and unvisited Os
-            if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m 
-            && !vis[nrow][ncol] && mat[nrow][ncol] == 'O') {
+            if(nrow >= 0 && nrow <n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && mat[nrow][ncol] == 'O') {
                 dfs(nrow, ncol, vis, mat, delrow, delcol); 
             }
         }
@@ -33,7 +32,6 @@ public:
             if(!vis[0][j] && mat[0][j] == 'O') {
                 dfs(0, j, vis, mat, delrow, delcol); 
             }
-            
             // last row 
             if(!vis[n - 1][j] && mat[n - 1][j] == 'O') {
                 dfs(n-1, j, vis, mat, delrow, delcol); 
@@ -61,5 +59,48 @@ public:
             }
         }
         return mat; 
+    }
+};
+
+
+// LeetCode Version: with no extra vis array
+
+class Solution {
+public:
+
+    void flooding(vector<vector<char>>& board, int i, int j){
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, 1, 0, -1}; 
+        
+        board[i][j] = 'V'; // gonecase
+        for(int k = 0; k < 4; k++){
+            int in = i + delrow[k];
+            int jn = j + delcol[k];
+            if(in >= 0 && jn >= 0 && in < board.size() && jn < board[0].size() && board[in][jn] == 'O'){
+                flooding(board, in, jn);
+            }
+        }
+    }
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        for(int i = 0; i < n; i++){
+            if(board[i][0] == 'O') flooding(board, i, 0);
+        }
+        for(int i = 0; i < n; i++){
+            if(board[i][m - 1] =='O') flooding(board, i, m - 1);
+        }
+        for(int j = 0; j < m; j++){
+            if(board[0][j] == 'O') flooding(board, 0, j);
+        }
+        for(int j = 0; j < m; j++){
+            if(board[n - 1][j] == 'O') flooding(board, n - 1, j);
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 'V') board[i][j] = 'O';
+                else if(board[i][j] == 'O') board[i][j] = 'X';
+            }
+        }
     }
 };
