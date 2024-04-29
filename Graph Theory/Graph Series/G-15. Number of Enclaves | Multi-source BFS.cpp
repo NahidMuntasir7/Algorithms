93be076f09d9378d8fa4b364cb935062db10420b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-29 20:48:21 +0600

    Update G-15. Number of Enclaves | Multi-source BFS.cpp

--- Graph Theory/Graph Series/G-15. Number of Enclaves | Multi-source BFS.cpp
+++ Graph Theory/Graph Series/G-15. Number of Enclaves | Multi-source BFS.cpp
@@ -58,3 +58,54 @@ class Solution {
         return cnt; 
     }
 };
+
+
+// LeetCode Version
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 || j == 0 || i == n - 1 || j == m - 1 ){
+                   if(grid[i][j] == 1){
+                       q.push({i, j});
+                       vis[i][j] = 1;
+                   }
+                }
+            }
+        }
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        while(!q.empty()){
+            int i_1 = q.front().first;
+            int j_1 = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++){
+                int nrow = i_1 + dx[i];
+                int ncol = j_1 + dy[i];
+                if(nrow < n && nrow >= 0 && ncol < m && ncol >= 0 && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+               if(grid[i][j] == 1 && vis[i][j] == 0){
+                       cnt++;
+               }
+            }
+        }
+        return cnt;
+    }
+};
