628ed9c92928ec9406475c7df62ef54ff31aa6d6
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-30 12:29:04 +0600

    Update G-16. Number of Distinct Islands | Constructive Thinking + DFS.cpp

--- Graph Theory/Graph Series/G-16. Number of Distinct Islands | Constructive Thinking + DFS.cpp
+++ Graph Theory/Graph Series/G-16. Number of Distinct Islands | Constructive Thinking + DFS.cpp
@@ -1,1 +1,51 @@
+// Problem Statement: Given a boolean 2D matrix grid of size N x M. You have to find the number of distinct islands where 
+// a group of connected 1s (horizontally or vertically) forms an island. 
+// Two islands are considered to be distinct if and only if one island is equal to another (not rotated or reflected).
 
+class Solution {
+  private:
+    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>> &grid, vector<pair<int, int>>& vec, int row0, int col0) {
+      // mark the cell as visited
+      vis[row][col] = 1;
+
+      // coordinates - base coordinates
+      vec.push_back({row - row0, col - col0});
+      int n = grid.size();
+      int m = grid[0].size();
+
+      // delta row and delta column
+      int delrow[] = {-1, 0, +1, 0}; 
+      int delcol[] = {0, -1, 0, +1}; 
+
+      // traverse all 4 neighbours
+      for (int i = 0; i < 4; i++) {
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        // check for valid unvisited land neighbour coordinates 
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+            dfs(nrow, ncol, vis, grid, vec, row0, col0);
+        }
+      }
+    }
+  public:
+    int countDistinctIslands(vector < vector < int >> & grid) {
+      int n = grid.size();
+      int m = grid[0].size();
+      vector<vector<int>> vis(n, vector<int> (m, 0));
+      set<vector<pair<int, int>>> st;
+
+      // traverse the grid
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          // if not visited and is a land cell
+          if (!vis[i][j] && grid[i][j] == 1) {
+            vector<pair<int, int>> vec;
+            dfs(i, j, vis, grid, vec, i, j);
+            // store in set
+            st.insert(vec);
+          }
+        }
+      }
+      return st.size();
+    }
+};
