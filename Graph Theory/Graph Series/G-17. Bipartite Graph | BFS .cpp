5df73ee975848b474ebfd5adfc95fc454b677239
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-04-30 18:05:29 +0600

    Update G-17. Bipartite Graph | BFS .cpp

--- Graph Theory/Graph Series/G-17. Bipartite Graph | BFS .cpp
+++ Graph Theory/Graph Series/G-17. Bipartite Graph | BFS .cpp
@@ -41,3 +41,58 @@ public:
 	    return true; 
 	}
 };
+
+
+// LeetCode - Is Graph Bipartite?
+
+
+class Solution {
+    
+    private:
+    bool bfs(int start, int color[], vector<vector<int>>& graph, int n){
+        queue<int> q;
+        q.push(start);
+
+        color[start] = 0;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+
+            for(auto i: graph[node]){
+                if(color[i] == -1){
+                    color[i] =! color[node];
+                    q.push(i);
+                }
+                else if(color[i] == color[node]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+
+        int color[n];
+
+        for(int i = 0; i < n; i++){
+            color[i] = -1;
+        }
+
+        // components of graph
+        for(int i = 0; i < n; i++){
+            if(color[i] == -1){
+                if(bfs(i, color, graph, n) == false){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
