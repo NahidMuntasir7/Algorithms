0698ff0b2b957b7836153c6cecb119ee82b972a1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-05-01 11:52:03 +0600

    Create G-20. Find Eventual Safe States - DFS.cpp

---
+++ Graph Theory/Graph Series/G-20. Find Eventual Safe States - DFS.cpp
@@ -0,0 +1,4 @@
+// Problem Statement: A directed graph of V vertices and E edges is given in the form of an adjacency list adj. 
+// Each node of the graph is labeled with a distinct integer in the range 0 to V - 1. 
+// A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from that node leads to a terminal node.
+// You have to return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
