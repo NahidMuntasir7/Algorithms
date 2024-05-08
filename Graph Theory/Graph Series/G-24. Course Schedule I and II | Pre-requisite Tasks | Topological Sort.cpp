3a872e4cd2e4d925b01d483bf65cd9b861d5c7fa
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-05-08 19:48:58 +0600

    Update G-24. Course Schedule I and II | Pre-requisite Tasks | Topological Sort.cpp

--- Graph Theory/Graph Series/G-24. Course Schedule I and II | Pre-requisite Tasks | Topological Sort.cpp
+++ Graph Theory/Graph Series/G-24. Course Schedule I and II | Pre-requisite Tasks | Topological Sort.cpp
@@ -41,3 +41,49 @@ public:
     }
 };
 
+
+// Course Schedule II: Same just return the ans vector
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+// You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+// For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+// Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. 
+// If it is impossible to finish all courses, return an empty array.
+
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+        vector<int> adj[V];
+        vector<int> indegree(V, 0);
+        vector<int> ans;
+
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size() == V) return ans;
+        return {};
+    }
+};
