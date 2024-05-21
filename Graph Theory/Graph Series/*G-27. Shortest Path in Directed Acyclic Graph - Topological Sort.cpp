d6b221cb4dbc91c5c7207ec898261179e11644f0
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-05-21 19:58:33 +0600

    Update *G-27. Shortest Path in Directed Acyclic Graph - Topological Sort.cpp

--- Graph Theory/Graph Series/*G-27. Shortest Path in Directed Acyclic Graph - Topological Sort.cpp
+++ Graph Theory/Graph Series/*G-27. Shortest Path in Directed Acyclic Graph - Topological Sort.cpp
@@ -1,1 +1,6 @@
+// Given a DAG, find the shortest path from the source to all other nodes in this DAG. In this problem statement, we 
+// have assumed the source vertex to be ‘0’. You will be given the weighted edges of the graph.
+
+// Note: What is a DAG ( Directed Acyclic Graph)?
+// A Directed Graph (containing one-sided edges) having no cycles is said to be a Directed Acyclic Graph.
 
