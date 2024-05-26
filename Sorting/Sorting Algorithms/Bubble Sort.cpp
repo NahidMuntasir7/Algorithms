aa8d1ce932510449324e559ee3a3ed6579330307
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-05-26 18:05:36 +0600

    Create Bubble Sort.cpp

---
+++ Sorting/Sorting Algorithms/Bubble Sort.cpp
@@ -0,0 +1,33 @@
+// normal
+
+void bubbleSort(int arr[], int n){
+    int i, j, temp;
+    for(i = 0; i < n - 1; i++){
+        for(j = 0; j < n - i - 1; j++){
+            if( arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            } 
+        }
+    }
+}
+
+// optimized
+
+void bubbleSort(int arr[], int n){
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped by inner loop, then break
+        if (swapped == false)
+            break;
+    }
+}
