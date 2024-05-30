0149a00ea862c24ac34fb166587035cbb9c21d3b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-05-30 12:49:21 +0600

    Update G-29. Word Ladder - I | Shortest Paths.cpp

--- Graph Theory/Graph Series/G-29. Word Ladder - I | Shortest Paths.cpp
+++ Graph Theory/Graph Series/G-29. Word Ladder - I | Shortest Paths.cpp
@@ -1,1 +1,12 @@
+// Given are the two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find the length of the shortest transformation sequence from startWord to targetWord.
+
+// In this problem statement, we need to keep the following conditions in mind:
+
+// A word can only consist of lowercase characters.
+// Only one letter can be changed in each transformation.
+// Each transformed word must exist in the wordList including the targetWord.
+// startWord may or may not be part of the wordList
+
+// Note:  If there’s no possible way to transform the sequence from startWord to targetWord return 0.
+
 
