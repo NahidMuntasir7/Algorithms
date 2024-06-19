3f84b988d3ed5085dee308b42a7bd6cb1c4b4bdf
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-19 13:28:41 +0600

    Update G-36. Shortest Distance in a Binary Maze.cpp

--- Graph Theory/Graph Series/G-36. Shortest Distance in a Binary Maze.cpp
+++ Graph Theory/Graph Series/G-36. Shortest Distance in a Binary Maze.cpp
@@ -89,3 +89,55 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+// LeetCode - Shortest Path in Binary Matrix
+// can visit only 0 and diagonal possible
+// normal BFS
+
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+       int n = grid.size();
+       int m = grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        if(grid[0][0]==0){
+        q.push({{0,0},1});
+        visited[0][0] = 1;
+
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int steps = q.front().second;
+            if(r == n - 1 and c == m - 1){
+                return steps;
+            }
+            
+            q.pop();
+            int dx[] = {-1,-1,-1,0,1,1,1,0};
+            int dy[] = {-1,0,1,1,1,0,-1,-1};
+
+            for(int i = 0; i < 8; i++){
+                int newr = r + dx[i];
+                int newc = c + dy[i];
+
+                if(newr >= 0 and newr < n and newc >= 0 and newc < m and grid[newr][newc] == 0 and visited[newr][newc] == 0){
+                    q.push({{newr,newc}, steps + 1});
+                    visited[newr][newc] = 1;
+                }
+            } 
+        }
+        }
+        return -1;
+
+    }
+};
