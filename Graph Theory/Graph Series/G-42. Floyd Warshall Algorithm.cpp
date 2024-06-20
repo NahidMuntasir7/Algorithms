a19639caf2ad4ee246c7640d78c6e5abf04150e5
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 10:14:54 +0600

    Update G-42. Floyd Warshall Algorithm.cpp

--- Graph Theory/Graph Series/G-42. Floyd Warshall Algorithm.cpp
+++ Graph Theory/Graph Series/G-42. Floyd Warshall Algorithm.cpp
@@ -23,7 +23,7 @@ public:
 		for (int k = 0; k < n; k++) {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+					matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);  // **** //
 				}
 			}
 		}
