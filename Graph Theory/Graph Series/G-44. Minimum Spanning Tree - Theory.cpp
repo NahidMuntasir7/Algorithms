2112d594e5ece2d351e98d14b26434a1c41ccf7b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 13:12:53 +0600

    Create G-44. Minimum Spanning Tree - Theory.cpp

---
+++ Graph Theory/Graph Series/G-44. Minimum Spanning Tree - Theory.cpp
@@ -0,0 +1,6 @@
+Spanning Tree:
+A spanning tree is a tree in which we have N nodes(i.e. All the nodes present in the original graph) and N-1 edges and all nodes are reachable from each other.
+
+MST:
+A minimum spanning tree (MST) is a subset of the edges of a connected, edge-weighted graph that connects all the vertices together without any cycles 
+and with the minimum possible total edge weight.
