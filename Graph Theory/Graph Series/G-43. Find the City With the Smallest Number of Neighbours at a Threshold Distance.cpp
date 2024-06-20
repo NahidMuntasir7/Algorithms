b028b2c76abfb78784a3630ecec660cb36385760
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 13:08:35 +0600

    Update G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp

--- Graph Theory/Graph Series/G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp
+++ Graph Theory/Graph Series/G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp
@@ -24,8 +24,7 @@ public:
 		for (int k = 0; k < n; k++) {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
-						continue;
+					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) continue;
 					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
 				}
 			}
