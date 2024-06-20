8b2ae9c69f14607db9e8e1ee5dc38bf2cdb62bb6
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 13:06:46 +0600

    Update G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp

--- Graph Theory/Graph Series/G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp
+++ Graph Theory/Graph Series/G-43. Find the City With the Smallest Number of Neighbours at a Threshold Distance.cpp
@@ -1,1 +1,65 @@
+// Problem Statement: There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi,weighti]  represents a 
+// bidirectional and weighted edge between cities fromi and toi, and given the integer distance Threshold. 
+// You need to find out a city with the smallest number of cities that are reachable through some path and whose distance is at most Threshold Distance, 
+// If there are multiple such cities, our answer will be the city with the greatest number.
 
+// Note: that the distance of a path, connecting cities i and j are equal to the sum of the edges' weights along that path.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	int findCity(int n, int m, vector<vector<int>>& edges,
+	             int distanceThreshold) {
+		vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
+		for (auto it : edges) {
+			dist[it[0]][it[1]] = it[2];
+			dist[it[1]][it[0]] = it[2];
+		}
+		for (int i = 0; i < n; i++) dist[i][i] = 0;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+						continue;
+					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+
+		int cntCity = n;
+		int cityNo = -1;
+		for (int city = 0; city < n; city++) {
+			int cnt = 0;
+			for (int adjCity = 0; adjCity < n; adjCity++) {
+				if (dist[city][adjCity] <= distanceThreshold)
+					cnt++;
+			}
+
+			if (cnt <= cntCity) {
+				cntCity = cnt;
+				cityNo = city;
+			}
+		}
+		return cityNo;
+
+	}
+};
+
+
+int main() {
+
+	int n = 4;
+	int m = 4;
+	vector<vector<int>> edges = {{0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1}};
+	int distanceThreshold = 4;
+
+	Solution obj;
+	int cityNo = obj.findCity(n, m, edges, distanceThreshold);
+	cout << "The answer is node: " << cityNo << endl;
+
+	return 0;
+}
