3c8a20b2c261be5c40c3945a39f99da3c662b9c8
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 10:07:20 +0600

    Create G-42. Floyd Warshall Algorithm.cpp

---
+++ Graph Theory/Graph Series/G-42. Floyd Warshall Algorithm.cpp
@@ -0,0 +1,68 @@
+// Problem Statement: The problem is to find the shortest distances between every pair of vertices in a given edge-weighted directed graph. 
+// The graph is represented as an adjacency matrix of size n*n. Matrix[i][j] denotes the weight of the edge from i to j. 
+// If Matrix[i][j]=-1, it means there is no edge from i to j.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+	void shortest_distance(vector<vector<int>>&matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j],
+					                   matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+
+
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 1e9) {
+					matrix[i][j] = -1;
+				}
+			}
+		}
+	}
+};
+
+
+int main() {
+
+	int V = 4;
+	vector<vector<int>> matrix(V, vector<int>(V, -1));
+	matrix[0][1] = 2;
+	matrix[1][0] = 1;
+	matrix[1][2] = 3;
+	matrix[3][0] = 3;
+	matrix[3][1] = 5;
+	matrix[3][2] = 4;
+
+	Solution obj;
+	obj.shortest_distance(matrix);
+
+	for (auto row : matrix) {
+		for (auto cell : row) {
+			cout << cell << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
