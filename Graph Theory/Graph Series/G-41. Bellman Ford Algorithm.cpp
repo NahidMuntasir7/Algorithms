c4c106edb08929713b4f5a96c5227f97b39283cf
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-06-20 09:59:01 +0600

    Update G-41. Bellman Ford Algorithm.cpp

--- Graph Theory/Graph Series/G-41. Bellman Ford Algorithm.cpp
+++ Graph Theory/Graph Series/G-41. Bellman Ford Algorithm.cpp
@@ -1,1 +1,5 @@
+// Problem Statement: Given a weighted, directed and connected graph of V vertices and E edges, Find the shortest distance of all the vertices from the source vertex S.
+
+// Note: If the Graph contains a negative cycle then return an array consisting of only -1.
+
 
