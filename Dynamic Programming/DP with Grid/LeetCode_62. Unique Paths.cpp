5a1e7e1e119a09974d24d5103700fc5ac761504b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-07-05 22:27:07 +0600

    Create LeetCode_62. Unique Paths.cpp

---
+++ Dynamic Programming/DP with Grid/LeetCode_62. Unique Paths.cpp
@@ -0,0 +1,23 @@
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
+// The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+
+class Solution {
+private:
+    int helpme(int i, int j, int m, int n, vector<vector<int>> &dp){
+        if (i == m - 1 and j == n - 1)   return 1;
+        if (i >= m or j >= n)    return 0;
+        if (dp[i][j] != -1)   return dp[i][j];
+        
+        int down = helpme(i + 1, j, m, n, dp);
+        int right = helpme(i, j + 1, m, n, dp);
+        
+        return dp[i][j] =  down + right;
+    }
+public:
+    int uniquePaths(int m, int n){
+        vector<vector<int>>dp(m + 1, vector<int>(n + 1, -1));
+        return helpme(0, 0, m, n, dp);
+    }
+};
