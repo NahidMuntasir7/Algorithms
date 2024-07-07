6ce0c7433c2e00c9b9b32c47ca859034fdb5b704
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-07-07 22:53:00 +0600

    Create CSES - Subordinates.cpp

---
+++ Trees/CSES - Subordinates.cpp
@@ -0,0 +1,45 @@
+// Given the structure of a company, your task is to calculate for each employee the number of their subordinates.
+
+#include<bits/stdc++.h>
+using namespace std;
+    
+#define ll long long
+#define endl '\n'
+const int N = 1e7 + 7;
+ 
+vector<int> tree[N];
+ 
+void dfs(int vertex, int par, vector<int> &ans){
+ 
+   int subords = 0;
+ 
+   for(int child : tree[vertex]){
+ 
+      if(child == par) continue;
+ 
+      dfs(child, vertex, ans);
+      subords += (1 + ans[child]);
+   }
+ 
+   ans[vertex] = subords;
+} 
+ 
+int main(){
+ 
+   int n; cin >> n;
+ 
+   vector<int> ans(n + 1);  // size na dile wrong dibe...
+ 
+   for(int i = 2; i <= n; i++){
+      int x;
+      cin >> x;
+      tree[x].push_back(i);
+      tree[i].push_back(x);
+   }
+ 
+   dfs(1, 0, ans);
+ 
+   for(int i = 1; i <= n; i++){
+      cout << ans[i] << " ";
+   }
+}
