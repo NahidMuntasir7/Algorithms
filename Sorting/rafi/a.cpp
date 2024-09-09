b7bcdaaac0f78b9966ca16977405e42346bdac2a
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-09-09 12:49:28 +0600

    Update a.cpp

--- Sorting/rafi/a.cpp
+++ Sorting/rafi/a.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.count(target - nums[i])){
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+            }
+            else mp[nums[i]] = i;
+        }
+        
+        return ans;
+    }      
+};
