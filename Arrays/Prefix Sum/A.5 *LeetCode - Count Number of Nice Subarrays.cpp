7e0ee3df79601baab4895512eb0b2840b81ea93c
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 21:42:05 +0600

    Update A.5 *LeetCode - Count Number of Nice Subarrays.cpp

--- Arrays/Prefix Sum/A.5 *LeetCode - Count Number of Nice Subarrays.cpp
+++ Arrays/Prefix Sum/A.5 *LeetCode - Count Number of Nice Subarrays.cpp
@@ -63,5 +63,22 @@ public:
 
 
 
+//   Binary Subarrays With Sum: Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+
+exactly same one............................. so there is a pattern in this type of number of subarray where you can't decide... so do map method or atmost
 
 
