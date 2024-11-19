9f3375042d05dd14686431e6c06d32d3a90977bd
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 16:54:49 +0600

    Update Next Greater Element.cpp

--- Arrays/Others/Next Greater Element.cpp
+++ Arrays/Others/Next Greater Element.cpp
@@ -5,8 +5,8 @@ public:
         stack<int> st;
 
         for(int i = 0; i < arr.size(); i++){
-            while(!st.empty() and arr[i] > arr[st.top()]){
-                ans[st.top()] = i - st.top();
+            while(!st.empty() and arr[i] > arr[st.top()]){ // smallers are stored in stack
+                ans[st.top()] = i - st.top(); // distance from the smaller
                 st.pop();
             }
             st.push(i);
