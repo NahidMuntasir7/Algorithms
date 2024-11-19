bc4478f38e2c6061bc971f121e72dfbab162de65
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 17:57:05 +0600

    Update A.1 LeetCode - Range Sum Query - Immutable.cpp

--- Arrays/Prefix Sum/A.1 LeetCode - Range Sum Query - Immutable.cpp
+++ Arrays/Prefix Sum/A.1 LeetCode - Range Sum Query - Immutable.cpp
@@ -1,1 +1,19 @@
+class NumArray {
+    vector<int> pre; // here...
+
+public:
+    NumArray(vector<int>& nums) {
+        pre.push_back(0);
+        
+        for(int i = 0; i < nums.size(); i++){
+            pre.push_back(pre.back() + nums[i]);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        right = right + 1, left = left + 1; // as 0 based given
+
+        return pre[right] - pre[left - 1];
+    }
+};
 
