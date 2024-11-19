f2c09369dd33a37dc9ee4ed8d772b3e0cbe69eb1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 19:24:44 +0600

    Create A.3 LeetCode - Count Vowel Strings in Ranges.cpp

---
+++ Arrays/Prefix Sum/A.3 LeetCode - Count Vowel Strings in Ranges.cpp
@@ -0,0 +1,29 @@
+// Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
+
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+
+        unordered_set<char> st= {'a', 'e', 'i', 'o', 'u'};
+
+        vector<int> arr(words.size(), 0);
+      
+        for(int i = 0; i < words.size(); i++){
+            if(st.count(words[i].front()) and st.count(words[i].back())) arr[i] = 1;
+        }
+        vector<int> pre;
+        pre.push_back(0);
+
+        for(int i = 0; i < arr.size(); i++){
+            pre.push_back(pre.back() + arr[i]);
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++){
+            int l = queries[i][0] + 1, r = queries[i][1] + 1; // 0 based
+            ans.push_back(pre[r] - pre[l - 1]);
+        }
+
+        return ans;
+    }
+};
