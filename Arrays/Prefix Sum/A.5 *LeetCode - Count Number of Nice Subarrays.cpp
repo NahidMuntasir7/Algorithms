7d7d583781c2663146e5b7caae3070fa97117861
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 21:45:12 +0600

    Update A.5 *LeetCode - Count Number of Nice Subarrays.cpp

--- Arrays/Prefix Sum/A.5 *LeetCode - Count Number of Nice Subarrays.cpp
+++ Arrays/Prefix Sum/A.5 *LeetCode - Count Number of Nice Subarrays.cpp
@@ -63,7 +63,7 @@ public:
 
 
 
-//   Binary Subarrays With Sum: Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+//   LeetCode - Binary Subarrays With Sum: Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
 
 A subarray is a contiguous part of the array.
 
