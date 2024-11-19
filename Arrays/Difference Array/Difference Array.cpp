831d26f98e80d575d24188cc1a4c884198f91b50
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 10:26:04 +0600

    Update Difference Array.cpp

--- Arrays/Difference Array/Difference Array.cpp
+++ Arrays/Difference Array/Difference Array.cpp
@@ -5,24 +5,25 @@ const int N = 105;
 int d[N], ans[N], a[N];
 
 int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
   
-  int n, q; cin >> n >> q;
-  for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-  }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
   
-  while (q--) {
-    int l, r, x; cin >> l >> r >> x;
-    d[l] += x;
-    d[r + 1] -= x;
-  }
+    int n, q; cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
   
-  for (int i = 1; i <= n; i++) {
-    ans[i] = ans[i - 1] + d[i];
-    cout << ans[i] + a[i] << ' ';
-  }
+    while (q--) {
+        int l, r, x; cin >> l >> r >> x;
+        d[l] += x;
+        d[r + 1] -= x;
+    }
   
-  return 0;
+    for (int i = 1; i <= n; i++) {
+        ans[i] = ans[i - 1] + d[i];
+        cout << ans[i] + a[i] << ' ';
+    }
+  
+    return 0;
 }
