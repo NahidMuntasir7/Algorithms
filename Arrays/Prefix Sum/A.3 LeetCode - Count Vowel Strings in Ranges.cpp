893853b51f3e543ea87a343db2749ddc6c40f6ae
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-19 19:27:02 +0600

    Update A.3 LeetCode - Count Vowel Strings in Ranges.cpp

--- Arrays/Prefix Sum/A.3 LeetCode - Count Vowel Strings in Ranges.cpp
+++ Arrays/Prefix Sum/A.3 LeetCode - Count Vowel Strings in Ranges.cpp
@@ -27,3 +27,31 @@ public:
         return ans;
     }
 };
+
+
+
+// less code
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+
+        unordered_set<char> st= {'a', 'e', 'i', 'o', 'u'};
+
+        vector<int> arr(words.size(), 0);
+        vector<int> pre;
+        pre.push_back(0);
+        
+        for(int i = 0; i < words.size(); i++){
+            if(st.count(words[i].front()) and st.count(words[i].back())) arr[i] = 1;
+             pre.push_back(pre.back() + arr[i]); // 
+        }
+        
+        vector<int> ans;
+        for(int i = 0; i < queries.size(); i++){
+            int l = queries[i][0] + 1, r = queries[i][1] + 1; // 0 based
+            ans.push_back(pre[r] - pre[l - 1]);
+        }
+
+        return ans;
+    }
+};
