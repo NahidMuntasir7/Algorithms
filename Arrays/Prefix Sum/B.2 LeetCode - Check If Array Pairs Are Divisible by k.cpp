79d25f1889b64823a72cd273db1a88f798caab58
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-20 16:20:23 +0600

    Update B.2 LeetCode - Check If Array Pairs Are Divisible by k.cpp

--- Arrays/Prefix Sum/B.2 LeetCode - Check If Array Pairs Are Divisible by k.cpp
+++ Arrays/Prefix Sum/B.2 LeetCode - Check If Array Pairs Are Divisible by k.cpp
@@ -1,1 +1,11 @@
+// Given an array of integers arr of even length n and an integer k.
 
+// We want to divide the array into exactly n / 2 pairs such that the sum of each pair is divisible by k.
+
+// Return true If you can find a way to do that or false otherwise.
+
+// Example 1:
+
+// Input: arr = [1,2,3,4,5,10,6,7,8,9], k = 5
+// Output: true
+// Explanation: Pairs are (1,9),(2,8),(3,7),(4,6) and (5,10).
