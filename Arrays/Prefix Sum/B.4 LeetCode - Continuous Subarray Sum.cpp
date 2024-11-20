717887cfff74cf220cda33ef331e1de81acc9d67
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-20 23:13:19 +0600

    Update B.4 LeetCode - Continuous Subarray Sum.cpp

--- Arrays/Prefix Sum/B.4 LeetCode - Continuous Subarray Sum.cpp
+++ Arrays/Prefix Sum/B.4 LeetCode - Continuous Subarray Sum.cpp
@@ -1,1 +1,16 @@
+// Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
 
+// A good subarray is a subarray where:
+// its length is at least two, and
+// the sum of the elements of the subarray is a multiple of k.
+
+// Note that:
+// A subarray is a contiguous part of the array.
+// An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+
+// Example 1:
+
+// Input: nums = [23,2,4,6,7], k = 6
+// Output: true
+// Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
