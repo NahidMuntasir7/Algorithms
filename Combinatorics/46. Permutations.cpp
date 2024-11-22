267551fd7165e5cd2ac6420f9602f73e6fc5e809
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-22 19:57:21 +0600

    Update 46. Permutations.cpp

--- Combinatorics/46. Permutations.cpp
+++ Combinatorics/46. Permutations.cpp
@@ -37,3 +37,28 @@ public:
         return ans; 
     }
 };
+
+
+
+// swap
+
+class Solution {
+public:
+     vector<vector<int>>v2;
+    void per(vector<int>& v,int l,int h){
+        // v2.clear();
+        if(l==h){
+            v2.push_back(v);
+        }else{
+            for(int i=l;i<h;i++){
+                swap(v[l],v[i]);
+                per(v,l+1,h);
+                swap(v[l],v[i]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+       per(nums,0,nums.size());
+        return v2;
+    }
+};
