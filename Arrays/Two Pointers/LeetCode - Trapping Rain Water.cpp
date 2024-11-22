5adba32ad741315c53c5ec8578c55bd4f5561cc5
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-22 17:26:19 +0600

    Update LeetCode - Trapping Rain Water.cpp

--- Arrays/Two Pointers/LeetCode - Trapping Rain Water.cpp
+++ Arrays/Two Pointers/LeetCode - Trapping Rain Water.cpp
@@ -1,3 +1,14 @@
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+// Example 1:
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+
+// last max er hishebe water count  (7) 1 2 (9) 4 2 -----> right max is >|
+//                                  | left max is >  <----- 3 5 (7) 2 4 (9)                 
+
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -18,3 +29,4 @@ public:
         return sum;
     }
 };
+
