815fab03355fcefc8e2dff89a5fcf1a50d1184ff
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-22 08:58:09 +0600

    Update CSES-Apple Division.cpp

--- Bit Manipulation/CSES-Apple Division.cpp
+++ Bit Manipulation/CSES-Apple Division.cpp
@@ -1,3 +1,16 @@
+There are n apples with known weights. Your task is to divide the apples into two groups so that the difference between the weights of the groups is minimal.
+Constraints
+1 <= n <= 20
+1 <= p_i <= 10^9
+
+Example
+Input:
+5
+3 2 7 4 1
+
+Output:
+1
+    
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
