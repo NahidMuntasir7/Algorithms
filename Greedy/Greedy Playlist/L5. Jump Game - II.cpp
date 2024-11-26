213a5874355feeb271e5399a6f80110d16cb6a3e
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-26 19:54:21 +0600

    Update L5. Jump Game - II.cpp

--- Greedy/Greedy Playlist/L5. Jump Game - II.cpp
+++ Greedy/Greedy Playlist/L5. Jump Game - II.cpp
@@ -12,3 +12,4 @@
 // Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
 
+// same as before... asking the minimum jump
