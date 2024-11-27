a7dc11b9cfac5920b4a560c26adca9a8d2388926
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-27 21:52:52 +0600

    Update L5. Jump Game - II.cpp

--- Greedy/Greedy Playlist/L5. Jump Game - II.cpp
+++ Greedy/Greedy Playlist/L5. Jump Game - II.cpp
@@ -13,3 +13,25 @@
 
 
 // same as before... asking the minimum jump
+
+
+// checking from a range the farthest i can go
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int near = 0, far = 0, jumps = 0;
+
+        while (far < nums.size() - 1) {
+            int farthest = 0;
+            for (int i = near; i <= far; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;        
+    }
+};
