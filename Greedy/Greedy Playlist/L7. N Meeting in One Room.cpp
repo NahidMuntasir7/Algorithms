d7fb670e9114dd17b29d6c0d0a4cf80aca147864
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-28 21:51:09 +0600

    Update L7. N Meeting in One Room.cpp

--- Greedy/Greedy Playlist/L7. N Meeting in One Room.cpp
+++ Greedy/Greedy Playlist/L7. N Meeting in One Room.cpp
@@ -6,3 +6,49 @@
 // Input:  N = 6,  start[] = {1,3,0,5,8,5}, end[] =  {2,4,5,7,9,9}
 // Output: 1 2 4 5
 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the maximum number of meetings
+void maxMeetings(int start[], int end[], int N) {
+    vector<pair<int, pair<int, int>>> meetings;
+
+    // Create a vector of {end time, {start time, index}}
+    for (int i = 0; i < N; i++) {
+        meetings.push_back({end[i], {start[i], i + 1}});
+    }
+
+    // Sort meetings by end time (primary) and index (secondary)
+    sort(meetings.begin(), meetings.end());
+
+    vector<int> selectedMeetings;
+    int lastEndTime = 0;
+
+    // Iterate over the meetings to find the maximum set
+    for (auto &meeting : meetings) {
+        if (meeting.second.first > lastEndTime) {
+            selectedMeetings.push_back(meeting.second.second);
+            lastEndTime = meeting.first;
+        }
+    }
+
+    // Print the order of selected meetings
+    for (int i : selectedMeetings) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+// Driver code
+int main() {
+    int N = 6;
+    int start[] = {1, 3, 0, 5, 8, 5};
+    int end[] = {2, 4, 5, 7, 9, 9};
+
+    maxMeetings(start, end, N);
+
+    return 0;
+}
+
+
