318cf5ef6a3723f9c58eba2e5ee38d3bf0b9d66b
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-28 10:37:16 +0600

    Create L6. Job Sequencing Problem.cpp

---
+++ Greedy/Greedy Playlist/L6. Job Sequencing Problem.cpp
@@ -0,0 +1,14 @@
+// You are given a set of N jobs where each job comes with a deadline and profit. The profit can only be earned upon completing the job within its deadline. Find the number of jobs done and the maximum profit that can be obtained. Each job takes a single unit of time and only one job can be performed at a time.
+
+// Examples
+// Example 1:
+
+//  {id, deadline, profit} given
+
+// Input: N = 4, Jobs = {(1,4,20),(2,1,10),(3,1,40),(4,1,30)}
+// Output: 2 60
+
+// Explanation: The 3rd job with a deadline 1 is performed during the first unit of time .The 1st job is performed during the second unit of time as its deadline is 4.
+// Profit = 40 + 20 = 60
+
+
