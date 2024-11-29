aa685d19a435a4385c8b50dbeeec0763af7c057e
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-29 20:04:33 +0600

    Update L10. Minimum number of platforms required in a railway station.cpp

--- Greedy/Greedy Playlist/L10. Minimum number of platforms required in a railway station.cpp
+++ Greedy/Greedy Playlist/L10. Minimum number of platforms required in a railway station.cpp
@@ -15,4 +15,38 @@
 // more than one plartform needed when it is intersecting
 // so need to find out maximum number of intersections
 
-// 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findPlatform(int arr[], int dep[], int n){
+
+    sort(arr, arr + n);
+    sort(dep, dep + n);
+
+    int plat_needed = 1, result = 1;
+    int i = 1, j = 0;
+
+    while (i < n && j < n) {
+        if (arr[i] <= dep[j]) {
+            plat_needed++;
+            i++;
+        }
+        else if (arr[i] > dep[j]) {
+            plat_needed--;
+            j++;
+        }
+        result = max(result, plat_needed);
+    }
+
+    return result;
+}
+
+int main(){
+
+    int arr[] = { 100, 300, 500 };
+    int dep[] = { 900, 400, 600 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << findPlatform(arr, dep, n);
+    return 0;
+}
