4af2c7ded0b9d6a9a4677d6834f2de82d7bb286d
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-30 11:54:34 +0600

    Update L13. Fractional Knapsack Algorithm.cpp

--- Greedy/Greedy Playlist/L13. Fractional Knapsack Algorithm.cpp
+++ Greedy/Greedy Playlist/L13. Fractional Knapsack Algorithm.cpp
@@ -1,1 +1,10 @@
+// Problem Statement: The weight of N items and their corresponding values are given. We have to put these items in a knapsack of weight W such that the total value obtained is maximized.
+// Note: We can either take the item as a whole or break it into smaller units.
+
+// Example:
+// Input: N = 3, W = 50, values[] = {100,60,120}, weight[] = {20,10,30}.
+
+// Output: 240.00
+// Explanation: The first and second items  are taken as a whole  while only 20 units of the third item is taken. Total value = 100 + 60 + 80 = 240.00
+
 
