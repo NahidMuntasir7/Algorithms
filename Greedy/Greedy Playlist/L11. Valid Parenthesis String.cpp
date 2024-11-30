777e6368dc1135b8797cbf8b531e6b3ca13101f7
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-11-30 11:25:06 +0600

    Update L11. Valid Parenthesis String.cpp

--- Greedy/Greedy Playlist/L11. Valid Parenthesis String.cpp
+++ Greedy/Greedy Playlist/L11. Valid Parenthesis String.cpp
@@ -1,1 +1,43 @@
+// Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+// The following rules define a valid string:
+// Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+// Any right parenthesis ')' must have a corresponding left parenthesis '('.
+// Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+// '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
 
+// Example 1:
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "(*)"
+// Output: true
+
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> st, star;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '*') star.push(i);
+            else if(s[i] == '(') st.push(i);
+            else if(s[i] == ')' and !st.empty()) st.pop();
+            else if(s[i] == ')' and st.empty()){
+                if(!star.empty()){
+                    star.pop();
+                }
+                else return false;
+            }
+        } 
+
+        while(!st.empty() and !star.empty()){
+            if(st.top() > star.top()) return false;
+            st.pop(); star.pop();
+        }
+        
+        if(st.empty()) return true;
+        return false;
+
+    }
+};
