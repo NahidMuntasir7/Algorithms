d197160750b5415c19f0f1d5350722e57a618ef1
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-12-08 09:42:21 +0600

    Update modular arithmetic.cpp

--- Number Theory/modular arithmetic.cpp
+++ Number Theory/modular arithmetic.cpp
@@ -1,1 +1,15 @@
+for negative numbers:
+a mod m = ((a % m) + m) % m
+
+Addition/Subtraction: For subtraction of numbers under modulo:
+(a - b) mod m = ((a mod m) - (b mod m) + m) mod m
+(a + b) mod m = ((a mod m) + (b mod m)) mod m
+
+Multiplication:
+(a * b) mod m = ((a mod m) * (b mod m)) mod m
+
+Division:
+a / b mod m = (a * b^(-1)) mod m
+
+
 
