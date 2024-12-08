48fd4d1c0697c026ca8056990d0897e3cd8f8c27
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-12-08 10:18:30 +0600

    Update prime problems.cpp

--- Number Theory/prime problems.cpp
+++ Number Theory/prime problems.cpp
@@ -1,9 +1,52 @@
-Check if a number is a Mersenne Prime
-Problem: Check if a number of the form (2 ^ n) - 1
-Approach: Check if  (2 ^ n) - 1 is prime using the isPrime function.
+// Check if a number is a Mersenne Prime
+// Problem: Check if a number of the form (2 ^ n) - 1
+// Approach: Check if  (2 ^ n) - 1 is prime using the isPrime function.
 
 
 bool isMersennePrime(int n) {
     int num = (1 << n) - 1;
     return isPrime(num);
 }
+
+
+// Prime Sum Problem
+// Approach: Use the Sieve of Eratosthenes to precompute primes up to n, then sum them.
+
+int sumOfPrimes(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    int sum = 0;
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime[i]) sum += i;
+    }
+    return sum;
+}
+
+
+// Prime Sum Pair
+
+void primeSumPair(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i <= n / 2; ++i) {
+        if (isPrime[i] && isPrime[n - i]) {
+            cout << i << " " << n - i << endl;
+            break;
+        }
+    }
+}
+
