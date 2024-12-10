c80720e3442c8b077c3d32a4e9fb3eb10158a7f9
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-12-10 21:50:46 +0600

    Update 2D Vectors.cpp

--- Others/2D Vectors.cpp
+++ Others/2D Vectors.cpp
@@ -1,1 +1,17 @@
+int main() {
+    vector<vector<int>> mat; // Declare a 2D vector
 
+
+    vector<vector<int>> mat2(3, vector<int>(4, 0)); // 3x4 matrix of 0s
+  
+    mat.push_back({1, 2, 3}); // Add rows
+    mat.push_back({4, 5, 6});
+    mat.push_back({7, 8, 9});
+
+    for (int i = 0; i < mat.size(); i++) {
+        for (int j = 0; j < mat[i].size(); j++) {
+            cout << mat[i][j] << " "; // Access elements
+        }
+        cout << endl;
+    }
+}
