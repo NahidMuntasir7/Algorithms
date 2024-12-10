831849a24b478c5feea764be540304ea54c60afa
Nahid Muntasir Rifat <106462821+NahidMuntasir7@users.noreply.github.com>
2024-12-10 21:51:46 +0600

    Update 2D Vectors.cpp

--- Others/2D Vectors.cpp
+++ Others/2D Vectors.cpp
@@ -2,8 +2,9 @@ int main() {
     vector<vector<int>> mat; // Declare a 2D vector
 
 
-    vector<vector<int>> mat2(3, vector<int>(4, 0)); // 3x4 matrix of 0s
-  
+    vector<vector<int>> mat1(3, vector<int>(4, 0)); // 3x4 matrix of 0s
+
+    
     mat.push_back({1, 2, 3}); // Add rows
     mat.push_back({4, 5, 6});
     mat.push_back({7, 8, 9});
@@ -12,6 +13,11 @@ int main() {
         for (int j = 0; j < mat[i].size(); j++) {
             cout << mat[i][j] << " "; // Access elements
         }
+        cout << endl;
+    }
+
+    for (auto row : mat1) {               // Output mat1
+        for (int val : row) cout << val << " ";
         cout << endl;
     }
 }
